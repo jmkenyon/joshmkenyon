abef07bae4b6bc6382a126c98fd6a582f9b7eeab
Josh Kenyon <joshuakenyon1@gmail.com>
2026-02-09 10:56:04 -0300

    changed user des

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -50,7 +50,7 @@ const Header = () => {
         transition={{ duration: 0.5, delay: 0.7 }}
         className="max-w-2xl mx-auto"
       >
-        Proficient in React, JavaScript, Node.js, Tailwind CSS, and more! I build responsive and
+        Proficient in React, Python, Tailwind CSS, and more! I build responsive and
         user-friendly web applications that deliver exceptional user
         experiences.
       </motion.p>
